81303bfb18920f06db23b2f5212176355ec31641
Your Namethoratswapnil061@gmail.com <you@example.com>
2023-03-21 16:50:53 +0530

    [STNO-1] Initial Commit

---
+++ GitHubBranchCreationAndMerging/src/main/java/com/csi/GitHubBranchCreationAndMergingApplication.java
@@ -0,0 +1,13 @@
+package com.csi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHubBranchCreationAndMergingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitHubBranchCreationAndMergingApplication.class, args);
+	}
+
+}
